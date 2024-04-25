3e98f9b4dcc11b8ddfc985c15be28d94370f17fb
shreyas-pedda <spedda@umass.edu>
2024-04-25 16:17:52 -0400

    Created code for stopping forward motion with ultrasound sensor.

--- RCCarProject.ino
+++ RCCarProject.ino
@@ -1,7 +1,9 @@
-#define LEFT_WIRE 15
-#define RIGHT_WIRE A14
-#define UP_WIRE A15
-#define DOWN_WIRE A16
+#define LEFT_WIRE A15
+#define RIGHT_WIRE A16
+#define UP_WIRE A13
+#define DOWN_WIRE A14
+#define TRIG 5
+#define ECHO 16
 
 void setup(){
   Serial.begin(9600);
@@ -9,16 +11,41 @@ void setup(){
   pinMode(RIGHT_WIRE, OUTPUT);
   pinMode(UP_WIRE, OUTPUT);
   pinMode(DOWN_WIRE, OUTPUT);
+  pinMode(TRIG, OUTPUT); // set up distance sensor pins
+  pinMode(ECHO, INPUT);
+  digitalWrite(TRIG, LOW);
+}
+
+// read distance sensor, return centimeters
+float readDistance() {
+  digitalWrite(TRIG, LOW); delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH); delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  unsigned long timeout = micros() + 26233L;
+  while((digitalRead(ECHO) == LOW) && (micros() < timeout));
+  unsigned long start_time = micros();
+  timeout= start_time + 26233L;
+  while((digitalRead(ECHO) == HIGH) && (micros() < timeout));
+  unsigned long lapse = micros() - start_time;
+  return lapse * 0.01716f;
 }
 
 void loop(){
-  digitalWrite(LEFT_WIRE, HIGH);
-  delay(1000);
-  digitalWrite(LEFT_WIRE, LOW);
-  digitalWrite(RIGHT_WIRE, HIGH);
-  delay(1000);
-  digitalWrite(RIGHT_WIRE, LOW);
-  delay(5000);
+  float dist = readDistance();
+  Serial.println(dist);
+  digitalWrite(UP_WIRE, HIGH);
+  if (dist < 20){
+    digitalWrite(UP_WIRE, LOW);
+  }
+
+//  digitalWrite(LEFT_WIRE, HIGH);
+//  delay(1000);
+//  digitalWrite(LEFT_WIRE, LOW);
+//  digitalWrite(RIGHT_WIRE, HIGH);
+//  delay(1000);
+//  digitalWrite(RIGHT_WIRE, LOW);
+//  delay(5000);
+//  
 //  digitalWrite(DOWN_WIRE, HIGH);
 //  delay(1000);
 //  digitalWrite(DOWN_WIRE, LOW);
