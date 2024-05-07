d57b38554e6d8bfeb4550c0b5d62db1aa1090951
shreyas-pedda <spedda@umass.edu>
2024-05-06 22:29:01 -0400

    Attempted the NeoPixel code and also got the joystick and all motor functions to work.

--- CrashAvoidanceRCCar.ino
+++ CrashAvoidanceRCCar.ino
@@ -1,31 +1,66 @@
+#include <Arduino.h>
+#include <esp_now.h>
 #include <WiFi.h>
+#include <Adafruit_NeoPixel.h>
 
-#define LEFT_WIRE 12 // White
-#define RIGHT_WIRE 13 // Purple
-#define UP_WIRE 27 // Brown
-#define DOWN_WIRE 14 // Yellow
-#define TRIG1 2
-#define ECHO1 15
-#define TRIG2 33
-#define ECHO2 25
-//#define TRIG3 
-//#define ECHO3 
+#define LEFT_WIRE 13 // Dark Blue - 1
+#define RIGHT_WIRE 12 // Turquoise - 2
+#define UP_WIRE 14 // Red - 3, yellow  25
+#define DOWN_WIRE 27 // Black - 4, black
+#define TRIG1 15
+#define ECHO1 2
+#define TRIG2 16
+#define ECHO2 4
+#define TRIG3 17
+#define ECHO3 5
 
+#define D1 32
+#define D0 33
+
+#define ringshow_noglitch() {delay(1);ring.show();delay(1);ring.show();}
+
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(16, D0, NEO_GRB + NEO_KHZ800);
+
+
+typedef struct joystick_input {
+  int x; // from -1800 to 1800
+  int y; // from -1800 to 1800
+  // deadzone from -110 to 100
+  bool control; // initial value false, clicking button flips boolean
+} joystick_input;
+
+joystick_input joystick_data;
 const int pwmFrequency = 200; // PWM frequency in Hz
 const int pwmResolution = 8;   // PWM resolution in bits (8-bit resolution for ESP32)
 
-int xJoystick = 0; // from -2048 to 2048
-int yJoystick = 0; // from -2048 to 2048
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&joystick_data, incomingData, sizeof(joystick_data));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("x: ");
+  Serial.println(joystick_data.x);
+  Serial.print("y: ");
+  Serial.println(joystick_data.y);
+  Serial.print("button: ");
+  Serial.println(joystick_data.control);
+  Serial.println();
 
-// deadzone from -20 to 20 is all 0
-
+}
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  esp_now_register_recv_cb(OnDataRecv);
+
   pinMode(LEFT_WIRE, OUTPUT);
   pinMode(RIGHT_WIRE, OUTPUT);
   pinMode(UP_WIRE, OUTPUT);
   pinMode(DOWN_WIRE, OUTPUT);
+//  Serial.println(WiFi.macAddress());
   
   pinMode(TRIG1, OUTPUT); // set up distance1 sensor pins
   pinMode(ECHO1, INPUT);
@@ -33,69 +68,106 @@ void setup(){
   pinMode(TRIG2, OUTPUT); // set up distance2 sensor pins
   pinMode(ECHO2, INPUT);
   digitalWrite(TRIG2, LOW);
-//  pinMode(TRIG3, OUTPUT); // set up distance3 sensor pins
-//  pinMode(ECHO3, INPUT);
-//  digitalWrite(TRIG3, LOW);
+  pinMode(TRIG3, OUTPUT); // set up distance3 sensor pins
+  pinMode(ECHO3, INPUT);
+  digitalWrite(TRIG3, LOW);
 
-  ledcSetup(0, pwmFrequency, pwmResolution);
-  ledcAttachPin(UP_WIRE, 0);
+  ledcSetup(1, pwmFrequency, pwmResolution);
+  ledcSetup(2, pwmFrequency, pwmResolution);
+  ledcSetup(3, pwmFrequency, pwmResolution);
+  ledcSetup(4, pwmFrequency, pwmResolution);      
+  ledcAttachPin(UP_WIRE, 1);
+  ledcAttachPin(DOWN_WIRE, 2);
+  ledcAttachPin(RIGHT_WIRE, 3);
+  ledcAttachPin(LEFT_WIRE, 4);    
+  ledcWrite(1, 0); 
+  ledcWrite(2, 0); 
+  ledcWrite(3, 0); 
+  ledcWrite(4, 0); 
+
+  ring.begin();
+  ring.setBrightness(32);
+  ring.clear(); // clear all pixels
+  ring.setPixelColor(0,0);
+  ringshow_noglitch(); 
 }
-
-#define QUEUE_SIZE 5
 
 // read distance sensor, return centimeters
 float readDistance(int TRIG, int ECHO) {
   digitalWrite(TRIG, LOW); delayMicroseconds(3);
   digitalWrite(TRIG, HIGH); delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
-  unsigned long timeout=micros()+26233L;
-  while((digitalRead(ECHO)==LOW)&&(micros()<timeout));
+  unsigned long timeout = micros() + 26233L;
+  while((digitalRead(ECHO) == LOW) && (micros() < timeout));
   unsigned long start_time = micros();
-  timeout=start_time+26233L;
-  while((digitalRead(ECHO)==HIGH)&&(micros()<timeout));
+  timeout = start_time + 26233L;
+  while((digitalRead(ECHO) == HIGH) && (micros() < timeout));
   unsigned long lapse = micros() - start_time;
-  return lapse*0.01716f;
+  return lapse * 0.01716f;
 }
 
 // calls readDistance a certain amount of time 
 float averageDistance(int TRIG, int ECHO){
   unsigned long totalLapse = 0;
-
   for (int i = 0; i < 5; i++) {
     totalLapse += readDistance(TRIG, ECHO);
   }
   return totalLapse / 5;
 }
 
-void loop(){
-  float dist1 = averageDistance(TRIG1, ECHO1);
-  delay(50);
-  float dist2 = averageDistance(TRIG2, ECHO2);
-  Serial.print("1: ");
-  Serial.println(dist1);
-  Serial.print("2: ");
-  Serial.println(dist2);
-  ledcWrite(0, 50);
-  if (dist1 < 20 || dist2 < 20){
-    ledcWrite(0, 0);
-  }
+void updateMotors(joystick_input joystick_data, float dist1, float dist2, float dist3){
+  // Check if any distance sensor reads below 20
+  bool obstacleDetected = (dist1 < 30) || (dist2 < 20) || (dist3 < 20);
 
-//  digitalWrite(LEFT_WIRE, HIGH);
-//  delay(1000);
-//  digitalWrite(LEFT_WIRE, LOW);
-//  digitalWrite(RIGHT_WIRE, HIGH);
-//  delay(1000);
-//  digitalWrite(RIGHT_WIRE, LOW);
-//  delay(5000);
-//  
-//  digitalWrite(DOWN_WIRE, HIGH);
-//  delay(1000);
-//  digitalWrite(DOWN_WIRE, LOW);
-//  delay(2000);
-//
-//  digitalWrite(UP_WIRE, HIGH);
-//  delay(1000);
-//  digitalWrite(UP_WIRE, LOW);
-//  delay(2000); t
+   // If obstacle detected, stop all motor movements
+   if (obstacleDetected && joystick_data.control) {
+     ledcWrite(1, 0); 
+     ledcWrite(2, 0); 
+     ledcWrite(3, 0); 
+     ledcWrite(4, 0); 
+     return; 
+   }
+
+  // Map joystick input to PWM duty cycle
+  int dutyX = map(abs(joystick_data.x), 0, 1800, 0, 255);
+  int dutyY = map(abs(joystick_data.y), 0, 1800, 0, 200);
+  
+  if (joystick_data.x < 0){
+    ledcWrite(3, 0);
+    ledcWrite(4, dutyX);
+  } 
+  else if (joystick_data.x == 0) {
+    ledcWrite(4, 0);
+    ledcWrite(3, 0);
+  }
+  else{
+    ledcWrite(4, 0);
+    ledcWrite(3, dutyX);
+  }
+  
+  if (joystick_data.y < 0){
+    ledcWrite(1, 0);
+    ledcWrite(2, dutyY);
+  } else if (joystick_data.y == 0){
+      ledcWrite(1, 0);
+      ledcWrite(2, 0);
+  }
+  else{
+    ledcWrite(2, 0);
+    ledcWrite(1, dutyY);
+  }
+}
+
+void loop(){
+//  float dist1 = averageDistance(TRIG1, ECHO1);
+//  float dist2 = averageDistance(TRIG2, ECHO2);
+//  float dist3 = averageDistance(TRIG3, ECHO3);
+//  updateMotors(joystick_data, dist1, dist2, dist3);
+
+  for(int i=0; i < ring.numPixels(); i++) {
+    ring.setPixelColor(i, ring.Color(255, 0, 0));  
+  }
+  ringshow_noglitch();
+
   
 }
---
+++ joystick_sender/joystick_sender.ino
@@ -0,0 +1,130 @@
+#define X 32
+#define Y 35
+#define BUTTON 14
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Arduino.h>
+
+uint8_t broadcastAddress1[] = {0xC4, 0xDD, 0x57, 0x8E, 0x29, 0x28};
+
+typedef struct joystick_input {
+  int x;
+  int y;
+  bool control;
+} joystick_input;
+
+joystick_input test;
+
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(X,INPUT);
+  pinMode(Y,INPUT);
+  analogReadResolution(12);
+  pinMode(BUTTON, INPUT_PULLUP);
+  
+ 
+  WiFi.mode(WIFI_STA);
+ 
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  esp_now_register_send_cb(OnDataSent);
+   
+  // register peer
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  // register first peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  
+  //attachInterrupt(digitalPinToInterrupt(BUTTON), buttonInterrupt, FALLING);
+}
+volatile unsigned long last_interrupt_time = 0;
+
+//void buttonInterrupt() {
+//  unsigned long interrupt_time = millis();
+//  // If interrupts come faster than 50ms, assume it's a bounce and ignore
+//  if (interrupt_time - last_interrupt_time > 50) {
+//    test.control = !test.control; // Toggle the button state
+//    esp_err_t result = esp_now_send(0, (uint8_t *)&test, sizeof(joystick_input));
+////    if (result == ESP_OK) {
+////      Serial.println("Sent with success");
+////    } else {
+////      Serial.println("Error sending the data");
+////    }
+//  }
+//  last_interrupt_time = interrupt_time;
+//}
+
+int zeroFilter(int val){
+  if(val < 50 && val > -50  ){
+    return 0;
+  }
+  if(val > 1800){
+    return 1800;
+  }
+  if(val < -1800){
+    return -1800;
+  }
+  return val;
+}
+
+bool safety;
+void loop() {
+  int xVal = 0;
+  int yVal = 0;
+  for(int i = 0; i < 15; i++){
+    xVal += zeroFilter(analogRead(X)-1984)/15;
+    yVal += zeroFilter((analogRead(Y)-1984)*-1)/15;
+  }
+  test.x = xVal;
+  test.y = yVal;
+
+  safety = digitalRead(BUTTON);
+  if (safety == 0){
+    test.control = !test.control;
+  }
+
+
+//  int buttonState =  analogRead(BUTTON);
+//
+//  if(buttonState == LOW){
+//    //delay(50);
+//    if(analogRead(BUTTON) == LOW){
+//      safety = !safety;
+//      test.button = safety;
+//      //delay(50);
+//    }
+//  }
+  Serial.println(test.control);
+
+  esp_err_t result = esp_now_send(0, (uint8_t *) &test, sizeof(joystick_input));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+}
